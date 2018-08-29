d7226c75ecf0157b4de37dad901aac726502fbdf
andrewallen <andrewallen@CSCT2552327X001.ad.georgiasouthern.edu>
2018-08-29 10:40:52 -0400

    Original

---
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -0,0 +1,14 @@
+package assignment;
+
+public class FirstClassInGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		//add a statement to display your name in the console
+		
+		//Then commit your changes
+
+	}
+
+}
