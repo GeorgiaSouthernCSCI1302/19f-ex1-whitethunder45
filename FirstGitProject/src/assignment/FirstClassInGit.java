8663447fa99867b736a8599607184da520246f89
jn04137 <jn04137@CEIT2553214X025.ad.georgiasouthern.edu>
2019-08-21 16:34:21 -0400

    first commit

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -3,7 +3,7 @@ package assignment;
 public class FirstClassInGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello, my name is Jonathan!");
 		
 		//add a statement to display your name in the console
 		
